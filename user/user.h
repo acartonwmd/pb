a1fa92a96dc865fc17c2ec8c3cc1147c0aff1295
Alec C. <alecc427@gmail.com>
2026-05-13 01:02:57 -0600

    Setup debug and release configs

---
+++ user/user.h
@@ -0,0 +1,18 @@
+#ifndef USER_H
+#define USER
+
+#include <string>
+
+class User;
+
+class User {
+    std::string name;
+    std::string password;
+    std::string username;
+
+    bool user_login(std::string uame, std::string password) { }
+
+    
+};
+
+#endif
